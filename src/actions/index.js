af3894504e19ff03d59310095959de278270b0dc
MrAlekhin <arty-hero@yandex.ru>
2018-06-06 01:45:09 -0400

    add cities list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '2ddfe694d41dbc2a7c01419644c5a89c';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
